fea91349aa805cae93193c3882e3a892637547f5
facundoiba95 <facundoiba95@gmail.com>
2022-11-18 12:01:53 -0300

    inicio de proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import GlobalStyle from './GlobalStyles/GlobalStyles';
+import { ToDo } from './toDo/toDo';
+
+function App() {
+  return (
+    <>
+     <ToDo/>
+     <GlobalStyle/>
+    </>
+  )
+   
+  ;
+}
+
+export default App;
